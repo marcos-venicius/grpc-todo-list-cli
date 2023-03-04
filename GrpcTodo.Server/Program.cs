018d974d7833ad5508debaa7bc74341f3e713845
Marcos Venicius <marcosdevelopment.business@gmail.com>
2023-03-04 20:07:57 -0300

    update server project

--- GrpcTodo.Server/Program.cs
+++ GrpcTodo.Server/Program.cs
@@ -1,7 +1,24 @@
 var builder = WebApplication.CreateBuilder(args);
 
+// builder.Services.AddControllers();
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 builder.Services.AddGrpc();
 
 var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+// app.UseAuthorization();
+
+// app.MapControllers();
 
 app.Run();
