39f4d0c37923a5a65df27642987fd16826e57617
Marcos Venicius <marcosdevelopment.business@gmail.com>
2023-03-04 20:12:11 -0300

    add serilog to server project

--- GrpcTodo.Server/Program.cs
+++ GrpcTodo.Server/Program.cs
@@ -1,6 +1,15 @@
+using Serilog;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // builder.Services.AddControllers();
+
+builder.Host.UseSerilog((context, configuration) =>
+{
+    configuration
+        .WriteTo.Console()
+        .WriteTo.File("Logs/server-.log", rollingInterval: RollingInterval.Day);
+});
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
